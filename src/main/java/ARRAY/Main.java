c984dcceb61482f2e97953db36817db3c99aa4fc
vikash-mdh <vikashkumar555550@gmail.com>
2024-07-14 23:34:45 +0530

    add array file

--- src/main/java/ARRAY/Main.java
+++ src/main/java/ARRAY/Main.java
@@ -1,5 +1,6 @@
 package ARRAY;
-// What is an array? An array is a data structure that stores a collection of elements, typically of the same type, in contiguous memory locations.
+// What is an array? An array is a data structure that stores a collection of elements,typically of the same type, in contiguous memory locations.
+// One-dimensional array (1D array): This is the simplest form of an array where elements are stored in a single row or column. For example, [1, 2, 3, 4, 5].
 
 
 public class Main {
